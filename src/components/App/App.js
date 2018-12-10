952c2344b6bfd8ce63686d0dd2818c5d9d6b07e2
Delagardi <delagardi@ymail.com>
2018-12-10 05:01:57 +0200

    Added delete method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,33 @@ import Content from '../content';
 import './app.css';
 
 class App extends Component {
-  todoData = [
-    {id: 1, label: 'First item with custom name'},
-    {id: 2, label: 'Second item is active'},
-    {id: 3, label: 'Third item example'}
-  ];
+  constructor() {
+    super();
 
+    this.state = {
+      todoData: [
+        {id: 1, label: 'First item with custom name'},
+        {id: 2, label: 'Second item is active'},
+        {id: 3, label: 'Third item example'}
+      ]
+    }
+  }
+
+  deleteTodo = (id) => {
+    this.setState( ({todoData}) => {
+      const index = todoData.findIndex( (element) => element.id === id );
+
+      const before = todoData.slice(0, index);
+      const after = todoData.slice(index+1);
+      const newArray = [...before, ...after];
+
+      return {
+        todoData: newArray
+      }
+    })
+
+  }
+  
   render() {
     return (
       <div className="container-fluid bg-main">
@@ -18,8 +39,8 @@ class App extends Component {
           <div className="row">
             <Sidebar/>
             <Content 
-              todoData={this.todoData}
-              onDeleted={ (id) => console.log('DeletedID:', id)}/>
+              todoData={ this.state.todoData }
+              onDeleted={ this.deleteTodo }/>
           </div>
         </div>
       </div>
